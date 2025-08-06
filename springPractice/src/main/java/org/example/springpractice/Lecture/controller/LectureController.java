a703d554628f3bccdfe48fc6a5b2e9b4b2792083
flionme <sunginkim142@gmail.com>
2025-08-06 12:47:35 +0900

    Board Entity, Repository code 작성

---
+++ springPractice/src/main/java/org/example/springpractice/Lecture/controller/LectureController.java
@@ -0,0 +1,5 @@
+package org.example.springpractice.Lecture.controller;
+
+public class LectureController
+{
+}
---
+++ springPractice/src/main/java/org/example/springpractice/Lecture/repository/LectureRepository.java
@@ -0,0 +1,5 @@
+package org.example.springpractice.Lecture.repository;
+
+public class LectureRepository
+{
+}
