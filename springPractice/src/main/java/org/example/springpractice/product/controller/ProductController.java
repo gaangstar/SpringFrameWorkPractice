526718c9e41eabcd0e209355bd9c698b0857a056
yhg <hghgy@hansung.ac.kr>
2025-08-06 11:24:26 +0900

    [Update] ProductController

--- springPractice/src/main/java/org/example/springpractice/product/controller/ProductController.java
+++ springPractice/src/main/java/org/example/springpractice/product/controller/ProductController.java
@@ -15,7 +15,7 @@ public class ProductController {
 
     //상품 등록
     @PostMapping("/register")
-    public ResponseEntity<ProductDto.ProductRsp> register(@RequestBody ProductDto.ProductReq dto) {
+    public ResponseEntity register(@RequestBody ProductDto.ProductReq dto) {
         productService.register(dto);
 
         return ResponseEntity.status(200).body("등록 성공");
@@ -23,8 +23,8 @@ public class ProductController {
 
     //상품 상세 조회
     @GetMapping("/read")
-    public ResponseEntity<ProductDto.ProductRsp> read(Integer idx) {
-        ProductDto.Product response = productService.read(idx);
+    public ResponseEntity read(Integer idx) {
+        ProductDto.ProductReq response = productService.read(idx);
 
         return ResponseEntity.status(200).body(response);
     }
@@ -32,16 +32,16 @@ public class ProductController {
 
     //상품 목록 조회
     @GetMapping("/list")
-    public ResponseEntity<ProductDto.ProductRsp> list() {
-        List<ProductDto.Product> response = productService.list();
+    public ResponseEntity list() {
+        List<ProductDto.ProductReq> response = productService.list();
 
         return ResponseEntity.status(200).body(response);
     }
 
     //상품 검색
     @GetMapping("/search")
-    public ResponseEntity<ProductDto.ProductRsp> search(String name) {
-        List<ProductDto.Product> response = productService.search(name);
+    public ResponseEntity search(String name) {
+        List<ProductDto.ProductReq> response = productService.search(name);
 
         return ResponseEntity.status(200).body(response);
     }
