625bb0d79c0e9577ffe65af0de094156ed261077
yhg <hghgy@hansung.ac.kr>
2025-08-06 12:50:42 +0900

    [Update] BoardService 게시글 조회

--- springPractice/src/main/java/org/example/springpractice/Board/service/BoardService.java
+++ springPractice/src/main/java/org/example/springpractice/Board/service/BoardService.java
@@ -17,4 +17,18 @@ public class BoardService {
     public void register(BoardDto.BoardReq dto) {
         boardRepository.save(dto);
     }
+
+    //게시글 상세 조회
+    public BoardDto.BoardRes read(Long id) {
+        return boardRepository.findById(id).map(BoardDto.BoardRes::from).orElse(null);
+    }
+
+    //게시글 목록 조회
+    public List<BoardDto.BoardRes> list() {
+        return boardRepository.findAll()
+                .stream()
+                .map(BoardDto.BoardRes::from)
+                .toList();
+    }
+
 }
