d01de82954618a0dff855c25fa94b85652d8d385
flionme <sunginkim142@gmail.com>
2025-08-06 12:33:37 +0900

    Board Entity, Repository code 작성

--- springPractice/src/main/java/org/example/springpractice/Board/model/Board.java
+++ springPractice/src/main/java/org/example/springpractice/Board/model/Board.java
@@ -1,5 +1,46 @@
 package org.example.springpractice.Board.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Board
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idx;
+    private String title;
+    private String content;
+    
+    public int getIdx()
+    {
+        return idx;
+    }
+    
+    public void setIdx(int idx)
+    {
+        this.idx = idx;
+    }
+    
+    public String getTitle()
+    {
+        return title;
+    }
+    
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+    
+    public String getContent()
+    {
+        return content;
+    }
+    
+    public void setContent(String content)
+    {
+        this.content = content;
+    }
 }
