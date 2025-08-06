fcc08c3295d27d56dc68e1d450fad3a1f1745497
yhg <hghgy@hansung.ac.kr>
2025-08-06 14:04:21 +0900

    Movie Entity 생성

--- springPractice/src/main/java/org/example/springpractice/Movie/model/Movie.java
+++ springPractice/src/main/java/org/example/springpractice/Movie/model/Movie.java
@@ -1,5 +1,48 @@
 package org.example.springpractice.Movie.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Movie
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String title;
+    private String description;
+    private String time;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
 }
