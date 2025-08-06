259a5515a62b41bbf6bd2ee6aa7089295f6bb743
atimaby28 <atimaby28@gmail.com>
2025-08-06 11:18:24 +0900

    Update : Entity and JpaRepositoty

--- springPractice/src/main/java/org/example/springpractice/product/repository/ProductRepository.java
+++ springPractice/src/main/java/org/example/springpractice/product/repository/ProductRepository.java
@@ -1,4 +1,13 @@
 package org.example.springpractice.product.repository;
 
-public interface ProductRepository {
+import org.example.springpractice.product.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+    List<Product> findByName(String name);
+    Optional<Product> findByDescription(String description);
+    Optional<Product> findByPrice(Integer price);
 }
