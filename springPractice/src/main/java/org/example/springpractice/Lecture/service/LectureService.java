89393f50d47120f6d7e89178be935ff3437427ba
flionme <sunginkim142@gmail.com>
2025-08-06 14:13:53 +0900

    LectureService 코드 등록

--- springPractice/src/main/java/org/example/springpractice/Lecture/service/LectureService.java
+++ springPractice/src/main/java/org/example/springpractice/Lecture/service/LectureService.java
@@ -9,6 +9,7 @@ import java.util.List;
 @Service
 public class LectureService
 {
+    //
     private LectureRepository lectureRepository;
     
     public LectureService(LectureRepository lectureRepository)
