f3fca798f96e9757b0b95ddf73d5534a79c2ef4e
yhg <hghgy@hansung.ac.kr>
2025-08-06 12:50:24 +0900

    [Update] BoardService 게시글 등록

--- springPractice/src/main/java/org/example/springpractice/Board/service/BoardService.java
+++ springPractice/src/main/java/org/example/springpractice/Board/service/BoardService.java
@@ -1,5 +1,20 @@
 package org.example.springpractice.Board.service;
 
-public class BoardService
-{
+import org.example.springpractice.Board.model.BoardDto;
+import org.example.springpractice.Board.repository.BoardRepository;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class BoardService {
+    private final BoardRepository boardRepository;
+
+    public BoardService(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+    //게시글 등록
+    public void register(BoardDto.BoardReq dto) {
+        boardRepository.save(dto);
+    }
 }
