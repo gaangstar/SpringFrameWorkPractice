2acf256a957c9643fb7f7d194f20255d3011a199
flionme <sunginkim142@gmail.com>
2025-08-06 11:14:54 +0900

    Service, Dto 작성

---
+++ springPractice/src/main/java/org/example/springpractice/product/model/ProductDto.java
@@ -0,0 +1,75 @@
+package org.example.springpractice.product.model;
+
+public class ProductDto
+{
+    public static class ProductReq
+    {
+        private Integer idx;
+        private String name;
+        private Integer price;
+        private Integer description ;
+        
+        public ProductReq from(Product Entity)
+        {
+            ProductDto.ProductReq dto = new ProductDto.ProductReq();
+            dto.setIdx(Entity.getIdx);
+            dto.setName(Entity.getName);
+            dto.setPrice(Entity.getPrice);
+            dto.setDescription(Entity.getDescription);
+            
+            return dto;
+        }
+        
+        public Product toEntity()
+        {
+            Product entity = new Product();
+            entity.setName(this.name);
+            entity.setPrice(this.price);
+            entity.setDescription(this.description );
+            
+            return entity;
+        }
+        
+        
+        public Integer getIdx()
+        {
+            return idx;
+        }
+        
+        public void setIdx(Integer idx)
+        {
+            this.idx = idx;
+        }
+        
+        public String getName()
+        {
+            return name;
+        }
+        
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+        
+        public Integer getPrice()
+        {
+            return price;
+        }
+        
+        public void setPrice(Integer price)
+        {
+            this.price = price;
+        }
+        
+        public Integer getDescription()
+        {
+            return description;
+        }
+        
+        public void setDescription(Integer description)
+        {
+            this.description = description;
+        }
+    }
+    
+}
--- springPractice/src/main/java/org/example/springpractice/product/service/ProductService.java
+++ springPractice/src/main/java/org/example/springpractice/product/service/ProductService.java
@@ -1,4 +1,56 @@
 package org.example.springpractice.product.service;
 
+import org.example.springpractice.product.model.Product;
+import org.example.springpractice.product.model.ProductDto;
+import org.example.springpractice.product.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
 public class ProductService {
+    public ProductService(ProductRepository productRepository)
+    {
+        this.ProductRepository = productRepository;
+    }
+    
+    public void register(ProductDto.ProductReq dto)
+    {
+        productRepository.register(dto.toEntity());
+    }
+    
+    public List<ProductDto.ProductReq> list()
+    {
+        List<ProductDto.ProductReq> result = productRepository.list();
+        
+        
+        return result.stream().map(ProductDto.ProductReq::from).toList();
+    }
+    
+    public ProductDto.ProductReq read(Integer idx)
+    {
+        Optional<ProductDto.ProductReq> result = productRepository.read(idx);
+        
+        if(result.isEmpty())
+        {
+            return null;
+        }
+        
+        Product entity = result.get();
+        
+        return ProductDto.ProductReq.from(entity);
+    }
+    
+    public List<ProductDto.ProductReq> search(String name)
+    {
+        List<Product> result = productRepository.findByName(name);
+        
+        return result.stream().map(ProductDto.ProductReq::from).toList();
+    }
+    
+    
+    
+    
 }
